859f43b6b2cb44ed2ec6bfd6effe75cfddf99e58
Jaydin <103458151+JustTryingToDoBetter@users.noreply.github.com>
2025-12-16 20:24:16 +0000

    updated api routes

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -2,6 +2,8 @@
  
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\EventStreamController;
 
 Route::get('/notes', [NoteController::class, 'index']);
 Route::post('/notes', [NoteController::class, 'store']);
+Route::get('/events', [EventStreamController::class, 'stream']);
